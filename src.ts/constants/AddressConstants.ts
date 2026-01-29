ca487604952bca6071f941cf5a5aae8ceca57fe1
Akshat Mittal <itsreallyakshat@gmail.com>
2026-01-29 20:58:33 +0530

    Addresses getter

--- src.ts/constants/AddressConstants.ts
+++ src.ts/constants/AddressConstants.ts
@@ -1,4 +1,25 @@
-type Address = `0x${string}`;
+import type { Address } from "viem";
+import {
+  mainnet,
+  optimism,
+  base,
+  arbitrum,
+  polygon,
+  blast,
+  zora,
+  worldchain,
+  ink,
+  soneium,
+  avalanche,
+  bsc,
+  celo,
+  sepolia,
+  baseSepolia,
+  arbitrumSepolia,
+  unichain,
+  monad,
+  unichainSepolia,
+} from "viem/chains";
 
 export class UnsupportedChainError extends Error {
   constructor(chainId?: number) {
@@ -8,98 +29,163 @@ export class UnsupportedChainError extends Error {
   }
 }
 
-const POOL_MANAGER_ADDRESSES: Record<number, Address> = {
-  1: "0x000000000004444c5dc75cB358380D2e3dE08A90",
-  130: "0x1F98400000000000000000000000000000000004",
-  10: "0x9a13F98Cb987694C9F086b1F5eB990EeA8264Ec3",
-  8453: "0x498581fF718922c3f8e6A244956aF099B2652b2b",
-  42161: "0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32",
-  137: "0x67366782805870060151383F4BbFF9daB53e5cD6",
-  81457: "0x1631559198A9e474033433b2958daBC135ab6446",
-  7777777: "0x0575338e4C17006aE181B47900A84404247CA30f",
-  480: "0xb1860D529182ac3BC1F51Fa2ABd56662b7D13f33",
-  57073: "0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32",
-  1868: "0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32",
-  43114: "0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32",
-  56: "0x28e2Ea090877bF75740558f6BFB36A5ffeE9e9dF",
-  42220: "0x288dc841A52FCA2707c6947B3A777c5E56cd87BC",
-  143: "0x188d586Ddcf52439676Ca21A244753fA19F9Ea8e",
-  1301: "0x00B036B58a818B1BC34d502D3fE730Db729e62AC",
-  11155111: "0xE03A1074c86CFeDd5C142C4F04F1a1536e203543",
-  84532: "0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408",
-  421614: "0xFB3e0C6F74eB1a21CC1Da29aeC80D2Dfe6C9a317",
+type SupportedChain = ReturnType<typeof getSupportedChains>[number]["id"];
+type Mapping<T> = Record<SupportedChain, T>;
+
+const POOL_MANAGER_ADDRESSES: Mapping<Address> = {
+  [mainnet.id]: "0x000000000004444c5dc75cB358380D2e3dE08A90",
+  [unichain.id]: "0x1f98400000000000000000000000000000000004",
+  [optimism.id]: "0x9a13f98cb987694c9f086b1f5eb990eea8264ec3",
+  [base.id]: "0x498581ff718922c3f8e6a244956af099b2652b2b",
+  [arbitrum.id]: "0x360e68faccca8ca495c1b759fd9eee466db9fb32",
+  [polygon.id]: "0x67366782805870060151383f4bbff9dab53e5cd6",
+  [blast.id]: "0x1631559198a9e474033433b2958dabc135ab6446",
+  [zora.id]: "0x0575338e4c17006ae181b47900a84404247ca30f",
+  [worldchain.id]: "0xb1860d529182ac3bc1f51fa2abd56662b7d13f33",
+  [ink.id]: "0x360e68faccca8ca495c1b759fd9eee466db9fb32",
+  [soneium.id]: "0x360e68faccca8ca495c1b759fd9eee466db9fb32",
+  [avalanche.id]: "0x06380c0e0912312b5150364b9dc4542ba0dbbc85",
+  [bsc.id]: "0x28e2ea090877bf75740558f6bfb36a5ffee9e9df",
+  [celo.id]: "0x288dc841A52FCA2707c6947B3A777c5E56cd87BC",
+  [monad.id]: "0x188d586ddcf52439676ca21a244753fa19f9ea8e",
+  // Testnets
+  [unichainSepolia.id]: "0x00b036b58a818b1bc34d502d3fe730db729e62ac",
+  [sepolia.id]: "0xE03A1074c86CFeDd5C142C4F04F1a1536e203543",
+  [baseSepolia.id]: "0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408",
+  [arbitrumSepolia.id]: "0xFB3e0C6F74eB1a21CC1Da29aeC80D2Dfe6C9a317",
 };
 
-const POSITION_MANAGER_ADDRESSES: Record<number, Address> = {
-  1: "0xbD216513d74C8cf14cf4747E6AaA6420FF64ee9e",
-  130: "0x4529A01c7A0410167c5740C487A8DE60232617bf",
-  10: "0x3C3Ea4B57a46241e54610e5f022E5c45859A1017",
-  8453: "0x7C5f5A4bBd8fD63184577525326123B519429bDc",
-  42161: "0xd88F38F930b7952f2DB2432Cb002E7abbF3dD869",
-  137: "0x1Ec2eBf4F37E7363FDfe3551602425af0B3ceef9",
-  81457: "0x4AD2F4CcA2682cBB5B950d660dD458a1D3f1bAaD",
-  7777777: "0xf66C7b99e2040f0D9b326B3b7c152E9663543D63",
-  480: "0xC585E0f504613b5fBf874F21Af14c65260fB41fA",
-  57073: "0x1b35d13a2E2528f192637F14B05f0Dc0e7dEB566",
-  1868: "0x1b35d13a2E2528f192637F14B05f0Dc0e7dEB566",
-  43114: "0xB74b1F14d2754AcfcbBe1a221023a5cf50Ab8ACD",
-  56: "0x7A4a5c919aE2541AeD11041A1AEeE68f1287f95b",
-  42220: "0xf7965f3981e4D5BC383BfBCb61501763e9068CA9",
-  143: "0x5b7eC4a94fF9beDb700fb82aB09d5846972F4016",
-  1301: "0xf969Aee60879C54bAAed9F3eD26147Db216Fd664",
-  11155111: "0x429ba70129df741B2Ca2a85BC3A2a3328e5c09b4",
-  84532: "0x4B2C77d209D3405F41a037Ec6c77F7F5b8e2ca80",
-  421614: "0xAc631556d3d4019C95769033B5E719dD77124BAc",
+const POSITION_MANAGER_ADDRESSES: Mapping<Address> = {
+  [mainnet.id]: "0xbd216513d74c8cf14cf4747e6aaa6420ff64ee9e",
+  [unichain.id]: "0x4529a01c7a0410167c5740c487a8de60232617bf",
+  [optimism.id]: "0x3c3ea4b57a46241e54610e5f022e5c45859a1017",
+  [base.id]: "0x7c5f5a4bbd8fd63184577525326123b519429bdc",
+  [arbitrum.id]: "0xd88f38f930b7952f2db2432cb002e7abbf3dd869",
+  [polygon.id]: "0x1ec2ebf4f37e7363fdfe3551602425af0b3ceef9",
+  [blast.id]: "0x4ad2f4cca2682cbb5b950d660dd458a1d3f1baad",
+  [zora.id]: "0xf66c7b99e2040f0d9b326b3b7c152e9663543d63",
+  [worldchain.id]: "0xc585e0f504613b5fbf874f21af14c65260fb41fa",
+  [ink.id]: "0x1b35d13a2e2528f192637f14b05f0dc0e7deb566",
+  [soneium.id]: "0x1b35d13a2e2528f192637f14b05f0dc0e7deb566",
+  [avalanche.id]: "0xb74b1f14d2754acfcbbe1a221023a5cf50ab8acd",
+  [bsc.id]: "0x7a4a5c919ae2541aed11041a1aeee68f1287f95b",
+  [celo.id]: "0xf7965f3981e4d5bc383bfbcb61501763e9068ca9",
+  [monad.id]: "0x5b7ec4a94ff9bedb700fb82ab09d5846972f4016",
+  // Testnets
+  [unichainSepolia.id]: "0xf969aee60879c54baaed9f3ed26147db216fd664",
+  [sepolia.id]: "0x429ba70129df741B2Ca2a85BC3A2a3328e5c09b4",
+  [baseSepolia.id]: "0x4b2c77d209d3405f41a037ec6c77f7f5b8e2ca80",
+  [arbitrumSepolia.id]: "0xAc631556d3d4019C95769033B5E719dD77124BAc",
 };
 
-const V4_SWAP_ROUTER_ADDRESSES: Record<number, Address> = {
-  1: "0xA4B6DB94b3017e2b7f17055Ded97D117ed5F551A",
-  130: "0x67C9C6050978c1582E4A633760B80CA2eff4015A",
-  10: "0x186EAA4941DF95c6058C69FB30dfE23C2827f9Ac",
-  8453: "0x15c40591096E938FE2A62515A7f4B8f4349D1DEE",
-  42161: "0xC0077d448203c71f6b18061C2E95409b386982BE",
-  137: "0x8b318E3CcF4495108E8Db61a5814f6bAE98e1465",
-  81457: "0xE8835Fff71cC8e2253C959683914f24d50Ac9699",
-  7777777: "0x1770E250A9b67E44206542148ed7C5Df38235003",
-  480: "0x3D9e82fb83cA3BDa838AF22d7F3964b25Df64EE7",
-  57073: "0xC0077d448203c71f6b18061C2E95409b386982BE",
-  1868: "0xC0077d448203c71f6b18061C2E95409b386982BE",
-  43114: "0x342F35aE81cd6743A4727CdD57e883C877a65aC2",
-  56: "0x9461852Ae90c5633207283762a1b5Db337FB3F44",
-  42220: "0xEcebD2fFb6e454ef467381a19b9C6170Bbc77e4d",
-  143: "0x87F6Dce8Cf52e21093a43e9819d650B35FE83080",
-  11155111: "0xf13D190e9117920c703d79B5F33732e10049b115",
-  1301: "0x9cD2b0a732dd5e023a5539921e0FD1c30E198Dba",
-  84532: "0x71cD4Ea054F9Cb3D3BF6251A00673303411A7DD9",
-  421614: "0xcD8D7e10A7aA794C389d56A07d85d63E28780220",
+const STATE_VIEW_ADDRESSES: Mapping<Address> = {
+  [mainnet.id]: "0x7ffe42c4a5deea5b0fec41c94c136cf115597227",
+  [unichain.id]: "0x86e8631a016f9068c3f085faf484ee3f5fdee8f2",
+  [optimism.id]: "0xc18a3169788f4f75a170290584eca6395c75ecdb",
+  [base.id]: "0xa3c0c9b65bad0b08107aa264b0f3db444b867a71",
+  [arbitrum.id]: "0x76fd297e2d437cd7f76d50f01afe6160f86e9990",
+  [polygon.id]: "0x5ea1bd7974c8a611cbab0bdcafcb1d9cc9b3ba5a",
+  [blast.id]: "0x12a88ae16f46dce4e8b15368008ab3380885df30",
+  [zora.id]: "0x385785af07d63b50d0a0ea57c4ff89d06adf7328",
+  [worldchain.id]: "0x51d394718bc09297262e368c1a481217fdeb71eb",
+  [ink.id]: "0x76fd297e2d437cd7f76d50f01afe6160f86e9990",
+  [soneium.id]: "0x76fd297e2d437cd7f76d50f01afe6160f86e9990",
+  [avalanche.id]: "0xc3c9e198c735a4b97e3e683f391ccbdd60b69286",
+  [bsc.id]: "0xd13dd3d6e93f276fafc9db9e6bb47c1180aee0c4",
+  [celo.id]: "0xbc21f8720babf4b20d195ee5c6e99c52b76f2bfb",
+  [monad.id]: "0x77395f3b2e73ae90843717371294fa97cc419d64",
+  // Testnets
+  [unichainSepolia.id]: "0xc199f1072a74d4e905aba1a84d9a45e2546b6222",
+  [sepolia.id]: "0xe1dd9c3fa50edb962e442f60dfbc432e24537e4c",
+  [baseSepolia.id]: "0x571291b572ed32ce6751a2cb2486ebee8defb9b4",
+  [arbitrumSepolia.id]: "0x9d467fa9062b6e9b1a46e26007ad82db116c67cb",
 };
 
-const PERMIT2_ADDRESS: Address = "0x000000000022D473030F116dDEE9F6B43aC78BA3";
+const QUOTER_ADDRESSES: Mapping<Address> = {
+  [mainnet.id]: "0x52f0e24d1c21c8a0cb1e5a5dd6198556bd9e1203",
+  [unichain.id]: "0x333e3c607b141b18ff6de9f258db6e77fe7491e0",
+  [optimism.id]: "0x1f3131a13296fb91c90870043742c3cdbff1a8d7",
+  [base.id]: "0x0d5e0f971ed27fbff6c2837bf31316121532048d",
+  [arbitrum.id]: "0x3972c00f7ed4885e145823eb7c655375d275a1c5",
+  [polygon.id]: "0xb3d5c3dfc3a7aebff71895a7191796bffc2c81b9",
+  [blast.id]: "0x6f71cdcb0d119ff72c6eb501abceb576fbf62bcf",
+  [zora.id]: "0x5edaccc0660e0a2c44b06e07ce8b915e625dc2c6",
+  [worldchain.id]: "0x55d235b3ff2daf7c3ede0defc9521f1d6fe6c5c0",
+  [ink.id]: "0x3972c00f7ed4885e145823eb7c655375d275a1c5",
+  [soneium.id]: "0x3972c00f7ed4885e145823eb7c655375d275a1c5",
+  [avalanche.id]: "0xbe40675bb704506a3c2ccfb762dcfd1e979845c2",
+  [bsc.id]: "0x9f75dd27d6664c475b90e105573e550ff69437b0",
+  [celo.id]: "0x28566da1093609182dff2cb2a91cfd72e61d66cd",
+  [monad.id]: "0xa222dd357a9076d1091ed6aa2e16c9742dd26891",
+  // Testnets
+  [unichainSepolia.id]: "0x56dcd40a3f2d466f48e7f48bdbe5cc9b92ae4472",
+  [sepolia.id]: "0x61b3f2011a92d183c7dbadbda940a7555ccf9227",
+  [baseSepolia.id]: "0x4a6513c898fe1b2d0e78d3b0e0a4a151589b1cba",
+  [arbitrumSepolia.id]: "0x7de51022d70a725b508085468052e25e22b5c4c9",
+};
 
-export function getPoolManagerAddress(chainId: number): Address {
-  const address = POOL_MANAGER_ADDRESSES[chainId];
-  if (!address) {
-    throw new UnsupportedChainError(chainId);
-  }
-  return address;
+const UNIVERSAL_ROUTER_ADDRESSES: Mapping<Address> = {
+  [mainnet.id]: "0x66a9893cc07d91d95644aedd05d03f95e1dba8af",
+  [unichain.id]: "0xef740bf23acae26f6492b10de645d6b98dc8eaf3",
+  [optimism.id]: "0x851116d9223fabed8e56c0e6b8ad0c31d98b3507",
+  [base.id]: "0x6ff5693b99212da76ad316178a184ab56d299b43",
+  [arbitrum.id]: "0xa51afafe0263b40edaef0df8781ea9aa03e381a3",
+  [polygon.id]: "0x1095692a6237d83c6a72f3f5efedb9a670c49223",
+  [blast.id]: "0xeabbcb3e8e415306207ef514f660a3f820025be3",
+  [zora.id]: "0x3315ef7ca28db74abadc6c44570efdf06b04b020",
+  [worldchain.id]: "0x8ac7bee993bb44dab564ea4bc9ea67bf9eb5e743",
+  [ink.id]: "0x112908dac86e20e7241b0927479ea3bf935d1fa0",
+  [soneium.id]: "0x4cded7edf52c8aa5259a54ec6a3ce7c6d2a455df",
+  [avalanche.id]: "0x94b75331ae8d42c1b61065089b7d48fe14aa73b7",
+  [bsc.id]: "0x1906c1d672b88cd1b9ac7593301ca990f94eae07",
+  [celo.id]: "0xcb695bc5d3aa22cad1e6df07801b061a05a0233a",
+  [monad.id]: "0x0d97dc33264bfc1c226207428a79b26757fb9dc3",
+  // Testnets
+  [unichainSepolia.id]: "0xf70536b3bcc1bd1a972dc186a2cf84cc6da6be5d",
+  [sepolia.id]: "0x3A9D48AB9751398BbFa63ad67599Bb04e4BdF98b",
+  [baseSepolia.id]: "0x492e6456d9528771018deb9e87ef7750ef184104",
+  [arbitrumSepolia.id]: "0xefd1d4bd4cf1e86da286bb4cb1b8bced9c10ba47",
+};
+
+export function getSupportedChains() {
+  return [
+    mainnet,
+    unichain,
+    optimism,
+    base,
+    arbitrum,
+    polygon,
+    blast,
+    zora,
+    worldchain,
+    ink,
+    soneium,
+    avalanche,
+    bsc,
+    celo,
+    monad,
+    unichainSepolia,
+    sepolia,
+    baseSepolia,
+    arbitrumSepolia,
+  ];
 }
 
-export function getPositionManagerAddress(chainId: number): Address {
-  const address = POSITION_MANAGER_ADDRESSES[chainId];
-  if (!address) {
+export function getUniswapContracts(chainId: number) {
+  if (!getSupportedChains().some((chain) => chain.id === chainId)) {
     throw new UnsupportedChainError(chainId);
   }
-  return address;
-}
 
-export function getPermit2Address(): Address {
-  return PERMIT2_ADDRESS;
-}
-
-export function getV4SwapRouterAddress(chainId: number): Address {
-  const address = V4_SWAP_ROUTER_ADDRESSES[chainId];
-  if (!address) {
-    throw new UnsupportedChainError(chainId);
-  }
-  return address;
+  return {
+    v4: {
+      poolManager: POOL_MANAGER_ADDRESSES[chainId as SupportedChain],
+      positionManager: POSITION_MANAGER_ADDRESSES[chainId as SupportedChain],
+      stateView: STATE_VIEW_ADDRESSES[chainId as SupportedChain],
+      quoter: QUOTER_ADDRESSES[chainId as SupportedChain],
+    },
+    utility: {
+      permit2: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
+      universalRouter: UNIVERSAL_ROUTER_ADDRESSES[chainId as SupportedChain],
+    },
+  };
 }
